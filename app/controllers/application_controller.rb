83657b338dd56c3659277904e3a4f884a02031a3
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-10-07 22:11:57 +0900

    Basic認証の導入とpostsコントローラの作成とルーティング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |userid, password|
+      userid == ENV["BASIC_AUTH_ID"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述
+    end
+  end
 end
