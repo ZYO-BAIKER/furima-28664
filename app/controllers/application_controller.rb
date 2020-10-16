41c848144bf61eb898e28d9319cd9a74706292d1
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-10-16 19:18:41 +0900

    usersテーブルにカラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devide_controller?
 
   private
 
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |userid, password|
       userid == ENV["BASIC_AUTH_ID"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 end
