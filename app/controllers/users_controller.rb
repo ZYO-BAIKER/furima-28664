c9764b7d99c648fa7bc74ed282bc65b51c50a91f
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-10-17 18:46:49 +0900

    Rubocopを実行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
-
   def edit
   end
 
   def update
-    current_user.update(user_params) #ログインしているユーザーの情報を更新
+    current_user.update(user_params) # ログインしているユーザーの情報を更新
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email) #「name」と「email」の編集を許可
+    params.require(:user).permit(:name, :email) # 「name」と「email」の編集を許可
   end
 end
