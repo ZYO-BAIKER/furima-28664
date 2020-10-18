e43b068beaa3baa3bfb3973977b465a783a878a3
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-10-18 18:23:08 +0900

    パスワードが半角英数字混合であるバリデーションの設定

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :purchases
+
+  validates :nickname, presence: true, length: { maximum: 40 }
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+
+  with_options presence: true do
+    validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: 'is invalid. Input half-width characters.' }
+    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
+    validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :birth_date
+  end
+end
