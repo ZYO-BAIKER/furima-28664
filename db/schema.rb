7ee7376d04b19f90b22ad3cc76755fe77376fcef
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-10-18 15:45:54 +0900

    birth_dateをdate型に修正

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_10_17_121229) do
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "birth_date", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
