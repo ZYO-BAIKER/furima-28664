c1d5f8652cc8bf7996ef18fb1acd3aad529bfd97
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-10-18 17:35:20 +0900

    Revert "ユーザー管理機能"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :configure_permitted_parameters, if: :devise_controller? # deviseに関するコントローラーの処理であれば、configure~メソッドを実行
-  # before_action :authenticate_user! # ログインしていない場合はログイン画面に遷移
 
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |userid, password|
-      userid == ENV['BASIC_AUTH_ID'] && password == ENV['BASIC_AUTH_PASSWORD'] # 環境変数を読み込む記述
+      userid == ENV["BASIC_AUTH_ID"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述
     end
-  end
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_date]) # sign_up（新規登録の処理）に対して、nicknameキーのパラメーターを新たに許可
   end
 end
