692d1bf41cd0926d1a51237b14a2abd4054e7e6c
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-10-18 18:41:51 +0900

    Revert "Merge pull request #3 from ZYO-BAIKER/ユーザー管理機能"
    This reverts commit 3241e2c4a34d2922ca600eee096418f31c7322b0, reversing
    changes made to b44afaa6a0710ddf053e642ab59ae02779fcba72.

--- app/controllers/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class UsersController < ApplicationController
-  def edit
-  end
-
-  def update
-    current_user.update(user_params) # ログインしているユーザーの情報を更新
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'items#index'
-  resources :users
-  resources :items
+  root to: 'posts#index' # localhost:3000 にアクセスした時に実行される処理
+  resources :posts, only: [:index]
 end
