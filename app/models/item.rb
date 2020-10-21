fbc1c010a6a0129d74d3e8956496855e9d99ad89
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-10-21 23:43:28 +0900

    バリデーションの設定
    newアクションにインスタンス変数
    不要なファイルが作成されないように（MiniImageAppでは必要のないファイル）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
+  has_one_attached :image
   belongs_to :user
   has_many :comments
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :price, presence: true, format: { with: /\A[a-z0-9]+\z/i }
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :postage_payer_id, presence: true
+  validates :prefecture_seller_id, presence: true
+  validates :send_date_id, presence: true
+  validates :description, presence: true
+  
+  end
 end
