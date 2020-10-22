c4a3f3e10d521317e4d680d10527d81a5bdb767b
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-10-23 01:24:18 +0900

    バリデーションを設定rev2　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,15 +3,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :image, presence: true
-  validates :name, presence: true
+  validates :image, :name, :description, presence: true
+
   validates :price, presence: true, format: { with: /\A[a-z0-9]+\z/i }
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :postage_payer_id, presence: true
-  validates :prefecture_seller_id, presence: true
-  validates :send_date_id, presence: true
-  validates :description, presence: true
+
+  validates :send_date_id, :prefecture_seller_id, :postage_payer_id, :condition_id, :category_id,  numericality: { other_than: 1 }
   
   end
 end
