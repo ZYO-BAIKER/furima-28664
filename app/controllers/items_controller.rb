d43a464c497c0798317d72d7ac62065a933cb791
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-10-25 23:47:50 +0900

    削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,16 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  # def edit
+  #   @item = Item.find(params[:id])
+  # end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
   private
 
   def item_params
