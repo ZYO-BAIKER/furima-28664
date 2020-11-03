85bac9d79e7134033b4212001e33386a7d98ce45
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-11-03 18:07:53 +0900

    Formオブジェクトの単体テストコード

---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  describe '#create' do
+    before do
+      @order_address = FactoryBot.build(:order_address)
+    end
+
+    it 'すべての値（建物名以外）が正しく入力されていれば保存できること' do
+      expect(@order_address).to be_valid
+    end
+    it 'postal_codeが空では保存できないこと' do
+      @order_address.postal_code = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+    end
+    it 'postal_codeはハイフンが無いと保存できないこと' do
+      @order_address.postal_code = 1234567
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+    end
+    it 'prefectureがid_1では保存できないこと' do
+      @order_address.prefecture_id = 1
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it 'cityが空では保存できないこと' do
+      @order_address.city = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("City can't be blank")
+    end
+    it 'house_numberが空では保存できないこと' do
+      @order_address.house_number = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("House number can't be blank")
+    end
+    it 'phone_numberが空では保存できないこと' do
+      @order_address.phone_number = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+    end
+    it 'phone_numberはハイフンがあったら保存できないこと' do
+      @order_address.phone_number = '090-1234567'
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number Input correctly")
+    end
+    it 'phone_numberは11桁以内でないと保存できないこと' do
+      @order_address.phone_number = '0901234568911'
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number Input correctly")
+    end
+  end
+end
