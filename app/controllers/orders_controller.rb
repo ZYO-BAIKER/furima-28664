c49f3fcf5f7c9685e7f160bdfb64a47448391cc0
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-11-05 19:05:19 +0900

    購入機能コントローラーの実装
    カード決済 params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,11 +7,10 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
-    # binding.pry
     if @order_address.valid?
-      # pay_item
+      pay_item
       @order_address.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render 'index'
     end
@@ -20,20 +19,19 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id).merge(token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
 
   def set_item
-    @item = Item.find(params[:item_id]) #メンターメモ[:item_id]orderのindexに@itemを渡す
+    @item = Item.find(params[:item_id]) # メンターメモ[:item_id]orderのindexに@itemを渡す
   end
 
-  # def pay_item
-  #   Payjp.api_key = "sk_test_6487e57b814574453233981a"  # PAY.JPテスト秘密鍵
-  #   Payjp::Charge.create(
-  #     amount: order_params[:number,:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number],  
-  #     card: order_params[:token],    # カードトークン
-  #     currency: 'jpy'                 # 通貨の種類（日本円）
-  #   )
-  # end
-
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,        #商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'             # 通貨の種類（日本円）
+    )
+  end
 end
