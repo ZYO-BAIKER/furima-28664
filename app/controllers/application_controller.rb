41e840ebf7628adca048965888def7f14ab16454
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-11-20 22:34:26 +0900

    環境変数の記述修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |userid, password|
-      userid == ENV['BASIC_AUTH_ID'] && password == ENV['BASIC_AUTH_PASSWORD'] # 環境変数を読み込む記述
+      userid == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD'] # 環境変数を読み込む記述
     end
   end
 
