baf97ef7e450e6aaf9621a89436cb83ceb1c6dbb
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-12-11 21:37:31 +0900

    searchアクションルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :users, only: [:edit, :update]
   resources :items do
+    collection do
+      get 'search'
+    end
     resources :orders, only: [:index, :create]
     resources :comments, only:[:create,:update,:destroy] do
       member do
