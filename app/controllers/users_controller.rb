f0b28177f69fe35401c9615fe7bf109db56e0d80
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-12-13 19:55:48 +0900

    showアクションをコントローラーに定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,10 @@ class UsersController < ApplicationController
   def update
     current_user.update(user_params) # ログインしているユーザーの情報を更新
   end
+
+  def show
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @items = user.items
+  end
 end
