e0dea56da24054724a7e87f11cca65d35eb8e7ec
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2020-12-18 23:37:59 +0900

    ユーザー情報の有無で処理分け

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  
+
   def google_oauth2
     authorizatiion
   end
@@ -7,6 +7,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
   def authorizatiion
-    @user = User.form_omniauth(request.env["omniauth.auth"])
+    sns_info = User.form_omniauth(request.env["omniauth.auth"])
+    @user = sns_info[:user]
+    if @user.persisted? #ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
+      sign_in_and_redirect @user, event: :authentication
+    else #ユーザー情報が未登録なので、新規登録画面へ遷移
+      @sns_id = sns_info[:sns].id
+      render template: 'devise/registrations/new'
+    end
   end
+
 end
